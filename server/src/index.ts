180ea63fb12b084a5eeb8d011947d53b3b41e1c1
Vladislav <vlad19930514@gmail.com>
2021-01-13 02:16:27 +0300

    eslintAdd

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 const app = express()
 const port = 9000
- 
-app.get('/', (req,res)=>res.send('hello world'))
 
+const one:number=1
+app.get('/', (req,res)=>res.send(`'hello world' ${one}`))
+
+ 
 app.listen(port)
 console.log(`[app]: http://localhost:${port}`)
