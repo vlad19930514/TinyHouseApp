20cd4062e0e33aa7f5b8d21f0651493a20045517
Vladislav <vlad19930514@gmail.com>
2021-01-13 19:42:03 +0300

    graphql

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,35 @@
 import express from 'express'
+import {ApolloServer} from 'apollo-server-express'
+import {schema} from './graphql'
+/* import bodyParser from 'body-parser'
+import {listings} from './listings' */
+
+
 const app = express()
-const port = 9000
+const port = 9000 
 
-const one:number=1
-app.get('/', (req,res)=>res.send(`'hello world' ${one}`))
+const server = new ApolloServer({schema})
+server.applyMiddleware({app, path:'/api'})
 
- 
 app.listen(port)
 console.log(`[app]: http://localhost:${port}`)
+
+
+
+
+/* app.get("/listings", (_req, res)=>{
+   return res.send(listings)
+})
+ */
+/* app.post("/delete-listing", (req,res)=>{
+    const id:string = req.body.id
+
+    for (let i=0; i<listings.length; i++){
+if (listings[i].id === id){
+    return res.send(listings.splice(i,1))
+}
+    }
+    return res.send("failed to delete")
+}) */
+
+ 
