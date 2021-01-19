ce886d4ff6e4fbe11d557b6a79b27edbd59d284a
Vladislav <vlad19930514@gmail.com>
2021-01-20 01:10:57 +0300

    GoogleAuth client and server side complete with cookies

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { render } from "react-dom";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import ApolloClient from "apollo-boost";
-import { ApolloProvider } from "@apollo/react-hooks";
-import { Affix, Layout } from "antd";
+import { ApolloProvider, useMutation } from "@apollo/react-hooks";
+import { Affix, Layout, Spin } from "antd";
 import {
   AppHeader,
   Home,
@@ -12,13 +12,26 @@ import {
   Listings,
   Login,
   NotFound
-} from "./sections";
+} from "./sections"; 
+import { AppHeaderSkeleton, ErrorBanner } from "./lib/components";
+import {LOG_IN} from './lib/graphql/mutations/'
+
+import {LogIn as LogInData, LogInVariables} from './lib/graphql/mutations/LogIn/__generated__/LogIn'
 import { Viewer } from "./lib/types";
 import * as serviceWorker from "./serviceWorker";
 import "./styles/index.css";
 
+
 const client = new ApolloClient({
-  uri: "/api"
+  uri: "/api",
+  request:async operation=>{
+    const token = sessionStorage.getItem("token")
+    operation.setContext({
+      headers:{
+        "X-CSRF-TOKEN": token ||""
+      }
+    })
+  }
 });
 
 const initialViewer: Viewer = {
@@ -31,10 +44,37 @@ const initialViewer: Viewer = {
 
 const App = () => {
   const [viewer, setViewer] = useState<Viewer>(initialViewer);
-
+  const [logIn,{error}]= useMutation<LogInData, LogInVariables>(LOG_IN,{
+    onCompleted:data=>{
+      if (data&&data.logIn){
+        setViewer(data.logIn)
+        if(data.logIn.token){
+          sessionStorage.setItem("token",data.logIn.token)
+        } else {
+          sessionStorage.removeItem("token")
+        }
+      }
+    }
+  })
+  const logInRef= useRef(logIn)
+useEffect(()=>{
+  logInRef.current()
+},[])
+if (!viewer.didRequest && !error) {
+  return (
+    <Layout className="app-skeleton">
+      <AppHeaderSkeleton />
+      <div className="app-skeleton__spin-section">
+        <Spin size="large" tip="Launching Tinyhouse" />
+      </div>
+    </Layout>
+  );
+}
+const logInErrorBannerElement= error ?<ErrorBanner description="We weren't able to verify if you were logged in. Please try again later"/>: null
   return (
     <Router>
       <Layout id="app">
+        {logInErrorBannerElement}
         <Affix offsetTop={0} className="app__affix-header">
           <AppHeader viewer={viewer} setViewer={setViewer} />
         </Affix>
